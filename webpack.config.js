9374d54115baa5d8605487d85165033445ca7f43
afrancis <albert.francis852@gmail.com>
2018-06-10 21:05:06 -0300

    configurando babel y webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    context: path.join(__dirname, 'src'),
+    entry: [
+        './client/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: [
+                    /node_modules/
+                ],
+                loader: 'babel-loader'
+            }            
+        ]
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, 'node_modules')
+        ]
+    }
+};
